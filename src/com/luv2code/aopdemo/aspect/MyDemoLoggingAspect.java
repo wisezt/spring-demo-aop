b8cb070314902dfc77aae5ec10abc5b47668c1b8
Ting <wisezt@hotmail.com>
2019-05-11 09:22:21 -0500

    Test @Pointcut

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,12 +2,18 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 
 @Component
 @Aspect
 public class MyDemoLoggingAspect {
+
+	@Pointcut("execution(* *(..))")
+	public void pointCut01(){}
+
+
 
 	// this where add all of our related advices for logging
 	
@@ -16,23 +22,23 @@ public class MyDemoLoggingAspect {
 	//@Before("execution(public * add*(com.luv2code.aopdemo.Account))")
 	
 	// wildcard cannot represent '.' (dot)
-	@Before("execution(public boolean com.luv2code.aopdemo.dao.*.*(..))")
+	@Before("pointCut01()")
 	public void beforeAddAccountAdvice(){
 		
 		System.out.println("\n=======>>> Executing @Before advice on method() Which the return type is boolean");
 	}
 
 
-	@Before("execution(* *(..))")
-	public  void beforeAddAccountAdvice2(){
-		System.out.println("\n=======>>> This is the beforeAddAccountAdvice2!!! ");
-
-
-	}
-
-    	@Before("execution(void *(..))")
-	public  void beforeAddAccountAdviceReturnVoid(){
-		System.out.println("\n=======>>> This is the beforeAddAccountAdviceReturnVoid!!! ");
-	}
+//	@Before("execution(* *(..))")
+//	public  void beforeAddAccountAdvice2(){
+//		System.out.println("\n=======>>> This is the beforeAddAccountAdvice2!!! ");
+//
+//
+//	}
+//
+//    	@Before("execution(void *(..))")
+//	public  void beforeAddAccountAdviceReturnVoid(){
+//		System.out.println("\n=======>>> This is the beforeAddAccountAdviceReturnVoid!!! ");
+//	}
 
 }
