f4627415b8aaae9dd34556900f761af4569b34bc
Ting <wisezt@hotmail.com>
2019-05-12 11:57:31 -0500

    Add @AfterRetuning

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,11 +2,14 @@ package com.luv2code.aopdemo.aspect;
 
 import com.luv2code.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Component
@@ -17,32 +20,28 @@ public class MyDemoLoggingAspect {
 	public void pointCut01(){}
 
 
-	@Before("pointCut01()")
-	public void beforeAddAccountAdvice(JoinPoint theJoinPoint){
-		
-
-		// display the method signature
-//		MethodSignature theSig = (MethodSignature) theJoinPoint.getSignature();
-//
-//		System.out.println("===> Signature:" + theSig);
-
-		// display method arguments
-//		for (Object obj : theJoinPoint.getArgs()){
-//
-//			if (obj instanceof Account){
-//				Account theAccount = (Account) obj;
-//
-//				System.out.println("===> Account name: " + theAccount.getName());
-//				System.out.println("===> Account level: " + theAccount.getLevel());
-//			}
+//	@Before("pointCut01()")
+//	public void beforeAddAccountAdvice(JoinPoint theJoinPoint){
 //
 //
-//		}
+//
+//	}
 
+    // Add the pointcut for returning the list of Accounts
+	@Pointcut("execution(* com.luv2code.aopdemo.MainDemoApp.*(..))")
+    public void pointcutReturnListAccounts(){
 
+    }
 
+    @AfterReturning(pointcut = "pointcutReturnListAccounts()", returning = "result")
+    public void afterReturningListAccounts(JoinPoint theJoinPoint, List<Account> result){
+	    System.out.println("===>AfterReturning name: afterReturningListAccounts");
+        System.out.println("JontPoint: " + theJoinPoint.getArgs());
+        System.out.println("Reuslt: " + result);
+        System.out.println("===>AfterReturning END");
 
-	}
+    }
+
 
 
 }
